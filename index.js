5b2aeb357fa32e665b072b0031bb47a8ce925cca
LilitVardanyan1992 <lvardanyan890@gmail.com>
2023-11-09 02:26:01 -0800

    created simple app using passport JS(local) for authentication

---
+++ index.js
@@ -0,0 +1,116 @@
+import express from "express";
+import path from "path";
+import bcrypt from "bcrypt";
+import session from "express-session";
+import passport from "passport";
+import passportLocal from "passport-local";
+
+let users = [];
+
+const app = express();
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(
+    new passportLocal.Strategy(
+        {
+            usernameField: "email",
+        },
+        async (email, password, done) => {
+            const user = users.find((user) => user.email === email);
+
+            if (user === undefined) {
+                return done(null, null, { message: "Incorrect email" });
+            }
+
+            if (await bcrypt.compare(password, user.password)) {
+                return done(null, user);
+            }
+
+            done(null, null, { message: "Incorrect password" });
+        }
+    )
+);
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+    done(
+        null,
+        users.find((user) => user.id === id)
+    );
+});
+
+app.get("/register", checkNotAuthentication, (req, res) => {
+    res.sendFile(path.resolve("views/register.html"));
+});
+
+app.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const hashedPwd = await bcrypt.hash(password, 10);
+
+    users.push({
+        id: `${Date.now()}_${Math.random()}`,
+        name,
+        email,
+        password: hashedPwd,
+    });
+
+    console.log(users);
+    res.redirect("/login");
+});
+
+app.get("/login", checkNotAuthentication, (req, res) => {
+    res.sendFile(path.resolve("views/login.html"));
+});
+
+app.post(
+    "/login",
+    passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/login",
+    })
+);
+
+app.get("/", checkAuthentication, (req, res) => {
+    res.sendFile(path.resolve("views/app.html"));
+});
+
+app.get("/logout", (req, res) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect("/");
+    });
+});
+
+function checkAuthentication(req, res, next) {
+    if (req.isAuthenticated() === false) {
+        // isAuthenticated ֆունկցիան passport-ն է ավելացնում, երբ Login է լինում յուզերը
+        return res.redirect("/register");
+    }
+    next();
+}
+
+function checkNotAuthentication(req, res, next) {
+    if (req.isAuthenticated() === true) {
+        return res.redirect("/");
+    }
+    next();
+}
+
+app.listen(process.env.PORT, () => {
+    console.log("Server started");
+});
